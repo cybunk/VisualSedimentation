a6e0df8868ad080aad1d4b915f0bd6bdad22310a
cybunk <admin@cybunk.com>
2013-06-25 20:47:27 -0600

    trail OTHERS

---
+++ examples/simple/custom_layout/_vs.chart.trailchart2.js
@@ -0,0 +1,93 @@
+(function ($) {
+
+$.fn._vs.chart.trailChart2 = function(_this,fn,options) {
+
+  this.init = function (_this){
+    console.log("Init my Custom Trail Chart ")
+    // Define the gravity (here 0)
+    _this.world.m_gravity   = new _this.phy.b2Vec2(0, 0);
+    // Save the physical layout setup 
+    _this.chartPhySetup     = {grounds:[],wall:[]}
+    // Setup the layout 
+    this.setupChartPhysics(_this);
+  };
+  
+  this.setupChartPhysics = function(_this){
+
+    // Here you could setup :
+    // - the incomming point 
+    // - alle physical element like grounds 
+     _this.settings.sedimentation.incoming.impulse =[]
+    var colSize       = (_this.settings.chart.width/_this.settings.data.model.length)    
+    for( var i = 0 ; i<_this.settings.data.model.length+1 ; i++) {
+        
+        var colXpos   = _this.settings.chart.x+(i*colSize);
+
+        // Define incomming points for tokens 
+        if(i<_this.settings.data.model.length){
+          _this.settings.sedimentation.incoming.point[i]={
+                                                    x:100,
+                                                    y:_this.settings.y+_this.settings.height/2+(i*50)-75
+                                                    }
+          _this.settings.sedimentation.incoming.target[i]={
+                                                    x:_this.settings.x+_this.settings.width-100,
+                                                    y:_this.settings.y+_this.settings.height/2
+                                                    }                                                    
+        }
+        
+    }
+  };
+
+  // Token feature creation call for this chart you could modify it  
+  this.token = function (_this,options){
+    var i = options;
+    var impulse =[
+                  {
+                    angle:260,
+                    power:0.2
+                  },
+                  {
+                    angle:0,
+                    power:0
+                  },
+                  {
+                    angle:100,
+                    power:0.2
+                  }
+                ]
+    var token = {
+              x:(_this.settings.sedimentation.incoming.point[i].x+(Math.random()*2)),
+              y:(_this.settings.sedimentation.incoming.point[i].y+(Math.random()*1)),
+              size:_this.settings.sedimentation.token.size.original,
+              category:i,
+              strokeStyle:"#fff",
+              lineWidth:1,  
+              targets:[{
+                  //  bizare x/2 or x ...
+                  x: _this.settings.sedimentation.incoming.target[i].x,
+                  y: _this.settings.sedimentation.incoming.target[i].y,
+                  dampingRatio:100,
+                  maxForce:1
+              }],
+              impulse:{
+                  //  bizare x/2 or x ...
+                  angle: impulse[i].angle,
+                  power: impulse[i].power
+              },
+              phy:{
+                density:10
+              }
+            }
+    return token; 
+  }
+
+ if (typeof(fn)!=undefined){
+    var result = this[fn](_this,options);  
+    if (typeof(result)!=undefined){
+      return result
+    }
+  }
+
+}
+
+})(jQuery);
